cec48ae34191d1a0c52c883a7b06a7a989115a63
Nikita Ivanov <nikita.i@reliz.com>
2022-09-06 17:37:47 +0400

    Change command add token to gitignore

--- src/main/kotlin/entry_point/Main.kt
+++ src/main/kotlin/entry_point/Main.kt
@@ -4,24 +4,28 @@ import dev.kord.core.Kord
 import dev.kord.core.event.interaction.ButtonInteractionCreateEvent
 import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
 import dev.kord.core.on
-import dev.kord.rest.builder.interaction.string
+import dev.kord.rest.builder.interaction.user
 import dice.DiceRoller
 import dice.Die
+import kotlinx.coroutines.flow.collect
 import roller.PersonalRoller
 import roller.Rollers
 
 suspend fun main(args: Array<String>) {
     val kord = Kord(TokenLoader())
 
-    kord.createGlobalChatInputCommand("ino", "talk with Ino") {
-        string("command", "performs a command") {
-            choice("create roller", "create roller")
+    kord.globalCommands.collect { it.delete() }
+
+    kord.createGlobalChatInputCommand("roller", "Создаёт персональный роллер для каждого пользователя") {
+        user("for", "Создать роллер для кого-то другого") {
+            required = false
         }
     }
     val rollers = Rollers()
     kord.on<GuildChatInputCommandInteractionCreateEvent> {
-        if (interaction.command.rootName != "ino") return@on
-        val roller = PersonalRoller(DiceRoller(Die(10)))
+        if (interaction.command.rootName != "roller") return@on
+        val user = runCatching { interaction.command.users.values.first().id }.getOrNull()
+        val roller = PersonalRoller(DiceRoller(Die(10)), user)
         roller.init(interaction)
         rollers.add(roller)
     }
